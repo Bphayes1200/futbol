b2d2be043790c61c873911e740feec18a7d7a6be
Amy Spears <amspears007@gmail.com>
2023-01-09 18:33:05 -0700

    Add updated instances to  initialize method

--- lib/stat_initialize.rb
+++ lib/stat_initialize.rb
@@ -4,9 +4,13 @@ require_relative 'team'
 require_relative 'game_team'
 
 class Stats
-  attr_reader :data_array,
-              :games,
+  attr_reader :games,
               :teams,
               :game_teams
-
+  def initialize(data)
+    @games = CSV.read(data[:games], headers: true, header_converters: :symbol).map{|game| Game.new(game)}
+    @teams = CSV.read(data[:teams], headers: true, header_converters: :symbol).map{|team| Team.new(team)}
+    @game_teams = CSV.read(data[:game_teams], headers: true, header_converters: :symbol).map{|game_team| GameTeam.new(game_team)}
+  end
+end
               
